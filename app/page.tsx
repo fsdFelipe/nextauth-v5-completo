ab8d36057d7e8cbb09ef21145ee5109ae1fdc16d
fsdFelipe <devfsdfelipe@gmail.com>
2024-02-13 22:27:31 -0300

    Projeto Inciado + config inicial

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <p>Auth v5</p>
+      <Button>Botão</Button>
+    </main>
+  );
+}
