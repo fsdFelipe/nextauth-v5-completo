7a123057d91a7222bae38ace7040648ec26ece9b
fsdFelipe <devfsdfelipe@gmail.com>
2024-02-18 20:59:07 -0300

    Funcionalidade logout

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import { auth } from '@/auth'
+import { auth, signOut } from '@/auth'
 
 const page = async () => {
     const session = await auth()
 
     return (
-        <div>Settings Page ! de {JSON.stringify(session)}</div>
+        <div>
+            Settings Page ! de
+            {JSON.stringify(session)}
+            <form action={async () => {
+                'use server'
+                await signOut()
+            }}>
+                <button type='submit'>
+                    Logout
+                </button>
+            </form>
+        </div>
     )
 }
 
