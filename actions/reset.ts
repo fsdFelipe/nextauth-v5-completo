7476138ebb87953f86a35fb09ff0fb8b67130951
fsdFelipe <devfsdfelipe@gmail.com>
2024-02-21 12:00:31 -0300

    Enviando email para resetar senha

--- actions/reset.ts
+++ actions/reset.ts
@@ -4,8 +4,8 @@ import * as z from "zod";
 
 import { ResetSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
-//import { sendPasswordResetEmail } from "@/lib/mail";
-//import { generatePasswordResetToken } from "@/lib/tokens";
+import { generatePasswordResetToken } from "@/lib/tokens";
+import { sendPasswordResetEmail } from "@/lib/mail";
 
 export const reset = async (values: z.infer<typeof ResetSchema>) => {
   const validatedFields = ResetSchema.safeParse(values);
@@ -22,11 +22,11 @@ export const reset = async (values: z.infer<typeof ResetSchema>) => {
     return { error: "Email not found!" };
   }
 
- // const passwordResetToken = await generatePasswordResetToken(email);
-  //await sendPasswordResetEmail(
-    //passwordResetToken.email,
-    //passwordResetToken.token,
-  //);
+  const passwordResetToken = await generatePasswordResetToken(email);
+  await sendPasswordResetEmail(
+    passwordResetToken.email,
+    passwordResetToken.token,
+  );
 
   return { success: "Reset email sent!" };
-};// 4:58:00
+};
