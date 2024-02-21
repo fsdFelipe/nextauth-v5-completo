a824e8246df0c945c82240028cdb23ea638bb4c1
fsdFelipe <devfsdfelipe@gmail.com>
2024-02-21 15:03:35 -0300

    Alterando a senha - finalzado

--- schemas/index.ts
+++ schemas/index.ts
@@ -22,3 +22,9 @@ export const RegisterSchema = z.object({
         message: 'Name é obrigatório'
     })
 })
+
+export const NewPasswordSchema = z.object({
+  password: z.string().min(6, {
+    message: "Minimum of 6 characters required",
+  }),
+});
